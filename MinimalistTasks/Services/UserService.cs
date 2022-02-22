e7e9be0075f994856be14af5716be50ac32deacc
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-02-22 17:10:24 -0300

    Add comments

--- MinimalistTasks/Services/UserService.cs
+++ MinimalistTasks/Services/UserService.cs
@@ -14,6 +14,11 @@ public class UserService
         _context = context;
     }
 
+    /// <summary>
+    /// Receives a UserDTO from the controller, converts it to User and inserts it into the Database
+    /// </summary>
+    /// <param name="userDto">userDto</param>
+    /// <returns>UserDTO</returns>
     public async Task<UserDTO> Insert(UserDTO userDto)
     {
         var user = FromDto(userDto);
@@ -22,11 +27,21 @@ public class UserService
         return ToDto(user);
     }
 
+    /// <summary>
+    /// Convert a User to UserDTO
+    /// </summary>
+    /// <param name="user">user</param>
+    /// <returns>UserDTO</returns>
     private static UserDTO ToDto(IUser user)
     {
         return new UserDTO(user);
     }
 
+    /// <summary>
+    /// Convert a UserDTO to User
+    /// </summary>
+    /// <param name="userDto">userDto</param>
+    /// <returns>User</returns>
     private static User FromDto(UserDTO userDto)
     {
         return new User
