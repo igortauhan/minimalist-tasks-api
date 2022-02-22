9374568cede19528728df7e74f20dc0c57c6b0f3
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-02-22 20:32:00 -0300

    Finish User CRUD

--- MinimalistTasks/Controller/UserController.cs
+++ MinimalistTasks/Controller/UserController.cs
@@ -18,10 +18,37 @@ public class UserController : ControllerBase
         _service = service;
     }
 
+    [HttpGet]
+    public async Task<IEnumerable<UserDTO>> GetAll()
+    {
+        var obj = await _service.GetAll();
+        return obj;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<UserDTO> GetUser(int id)
+    {
+        var obj = await _service.GetUser(id);
+        return obj;
+    }
+
     [HttpPost]
     public async Task<UserDTO> Insert([FromBody] UserDTO userDto)
     {
         var obj = await _service.Insert(userDto);
         return obj;
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<UserDTO> Update(int id, [FromBody] UserDTO userDto)
+    {
+        var obj = await _service.Update(id, userDto);
+        return obj;
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task Delete(int id)
+    {
+        await _service.Delete(id);
     }
 }
