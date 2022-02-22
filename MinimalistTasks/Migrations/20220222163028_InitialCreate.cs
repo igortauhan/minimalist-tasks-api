423c72099c1b4a288a873e14a3456c7d1aacf5b4
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-02-22 13:37:54 -0300

    Fix database connection string. Initial migration to Code-first.

--- MinimalistTasks/Migrations/20220222163028_InitialCreate.cs
+++ MinimalistTasks/Migrations/20220222163028_InitialCreate.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace MinimalistTasks.Migrations
 {
-    public partial class FirstMigration : Migration
+    public partial class InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
