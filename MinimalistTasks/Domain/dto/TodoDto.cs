7fd61b67ac856841ca3d9fa988ca575c04a0c070
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-02-23 09:39:19 -0300

    Fix TodoDto implementation

--- MinimalistTasks/Domain/dto/TodoDto.cs
+++ MinimalistTasks/Domain/dto/TodoDto.cs
@@ -1,4 +1,4 @@
-using MinimalistTasks.Domain.Model;
+using MinimalistTasks.Domain.Interface;
 
 namespace MinimalistTasks.Domain.Dto;
 
@@ -9,19 +9,17 @@ public class TodoDto
     public DateTime CreationDate { get; set; }
     public bool IsCompleted { get; set; }
     public int UserId { get; set; }
-    public User User { get; set; }
 
     public TodoDto()
     {
     }
 
-    public TodoDto(int? todoId, string? text, DateTime creationDate, bool isCompleted, int userId, User user)
+    public TodoDto(ITodo todo)
     {
-        TodoId = todoId;
-        Text = text;
-        CreationDate = creationDate;
-        IsCompleted = isCompleted;
-        UserId = userId;
-        User = user;
+        TodoId = todo.TodoId;
+        Text = todo.Text;
+        CreationDate = todo.CreationDate;
+        IsCompleted = todo.IsCompleted;
+        UserId = todo.UserId;
     }
 }
