9639f169a2102ccc0940cbc87d75c2a2d6e18b77
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-02-26 22:09:54 -0300

    Finish User Unit Tests

--- MinimalistTasks.UnitTest/UserTests.cs
+++ MinimalistTasks.UnitTest/UserTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using MinimalistTasks.Controller;
@@ -33,7 +35,7 @@ public class UserTests
             Name = "Rob",
             Email = "rob@rob.com"
         });
-        
+
         // Act
         var result = await _controller.GetUser(1);
 
@@ -41,5 +43,76 @@ public class UserTests
         Assert.That(result.UserId, Is.EqualTo(1));
         Assert.That(result.Name, Does.Contain("rob").IgnoreCase);
         Assert.That(result.Email, Is.EqualTo("rob@rob.com"));
+    }
+
+    [Test]
+    public async Task GetAllAsync_WhenCalled_ShouldReturnAListOfUserDto()
+    {
+        // Arrange
+        _userService.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<UserDto>
+        {
+            new UserDto(),
+            new UserDto(),
+            new UserDto()
+        });
+
+        // Act
+        var result = await _controller.GetAll();
+
+        // Assert
+        Assert.That(result.Count(), Is.EqualTo(3));
+    }
+
+    [Test]
+    public async Task InsertAsync_WhenCalled_ShouldInsertAUserAndReturnAUserDto()
+    {
+        // Arrange
+        var user = new UserDto
+        {
+            UserId = 1,
+            Name = "Rob",
+            Email = "rob@rob.com"
+        };
+        _userService.Setup(x => x.InsertAsync(user)).ReturnsAsync(new UserDto
+        {
+            UserId = 1,
+            Name = "Rob",
+            Email = "rob@rob.com"
+        });
+
+        // Act
+        var result = await _controller.Insert(user);
+
+        // Assert
+        Assert.That(result.UserId, Is.EqualTo(1));
+        Assert.That(result.Name, Is.EqualTo("rob").IgnoreCase);
+        Assert.That(result.Email, Is.EqualTo("rob@rob.com").IgnoreCase);
+    }
+
+    [Test]
+    public async Task UpdateAsync_WhenCalled_ShouldUpdateTheUserDataAndReturnAUserDto()
+    {
+        // Arrange
+        var oldUser = new UserDto
+        {
+            UserId = 1,
+            Name = "Rob",
+            Email = "rob@rob.com"
+        };
+        var newUser = new UserDto
+        {
+            UserId = 1,
+            Name = "Rob Less",
+            Email = "robless@rob.com"
+        };
+        _userService.Setup(x => x.UpdateAsync(1, oldUser)).ReturnsAsync(newUser);
+
+        // Act
+        var result = await _controller.Update(1, oldUser);
+
+        // Assert
+        Assert.That(result.UserId, Is.EqualTo(1));
+        Assert.That(result.Name, Is.EqualTo("Rob Less").IgnoreCase);
+        Assert.That(result.Email, Is.EqualTo("robless@rob.com").IgnoreCase);
     }
 }
